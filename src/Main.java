88dcee3b097e923915259e0571f20caf1f4c5c3f
user <Java4QA@i.ua>
2018-05-09 14:01:12 +0300

    Added the commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int a, b;
-        String natasha;
+        String natasha; // Текстовая переменная Наташа
         a=5;
         b=6;
         System.out.println(a + b);
