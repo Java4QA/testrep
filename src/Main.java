0309178331afc181535291109e69f52a42c1460f
user <Java4QA@i.ua>
2018-05-09 13:26:02 +0300

    New programm

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int a, b;
+        String natasha;
         a=5;
         b=6;
         System.out.println(a + b);
+        natasha = "Oleg";
+        System.out.println(natasha);
     }
+    
 }
